c160eff795cdf5c1a742f004d80758088687a781
Mete Ergin <mete.ergin@outlook.com>
2021-08-28 07:49:05 +0300

    First commit.

---
+++ src/main/java/com/meteergin/mediumspringbootsecurity/MediumSpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.meteergin.mediumspringbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumSpringbootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumSpringbootSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meteergin/mediumspringbootsecurity/MediumSpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteergin.mediumspringbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumSpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
